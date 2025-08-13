da458ab832e5e6f057ce2d99126f1418717c0924
GideonAkpenamawu <gideon.akpenamawu@st.vvu.edu.gh>
2025-08-13 10:24:33 +0000

    Add files via upload

---
+++ Calculation of voltage using Ohm's Law.py
@@ -0,0 +1,17 @@
+# Calculation of voltage using Ohm's Law
+
+import os
+
+# Get user input for current (I) and resistance (R)
+print("Ohm's Law: V = I * R")
+i = float(input("Enter the current (I): "))
+r= float(input("Enter the resistance (R): "))
+
+# Calculate the Voltage using Ohm's Law: V = I * R
+
+v=i*r
+
+# print the result
+print(f"The voltage is: {v} V")
+# Argument Sign Script
+4
